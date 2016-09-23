a8b4e7e35d247701047dc3ec4828040bff4643ef
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-09-23 17:19:29 +1000

    Pascal casing the variables

--- Program.fs
+++ Program.fs
@@ -19,39 +19,39 @@ open MathNet.Numerics.LinearAlgebra
 [<Measure>] type dpi
 
 module QuerySource =
-    type film = { id : int; name : string; releaseYear : int; gross : Nullable<float> }
-                override x.ToString() = sprintf "%s (%i)" x.name x.releaseYear
-    type actor = { id : int; firstName : string; lastName : string }
-                 override x.ToString() = sprintf "%s, %s" x.lastName x.firstName
-    type filmActor = { filmId : int; actorId : int }
+    type Film = { Id : int; Name : string; ReleaseYear : int; Gross : Nullable<float> }
+                override x.ToString() = sprintf "%s (%i)" x.Name x.ReleaseYear
+    type Actor = { Id : int; FirstName : string; LastName : string }
+                 override x.ToString() = sprintf "%s, %s" x.LastName x.FirstName
+    type FilmActor = { FilmId : int; ActorId : int }
     
     let films =
         [
-        { id = 1; name = "The Terminator"; releaseYear = 1984; gross = Nullable 38400000.0 }
-        { id = 2; name = "Predator"; releaseYear = 1987; gross = Nullable 59735548.0 }
-        { id = 3; name = "Commando"; releaseYear = 1985; gross = Nullable<float>() }
-        { id = 4; name = "The Running Man"; releaseYear = 1987; gross = Nullable 38122105.0 }
-        { id = 5; name = "Conan the Destroyer"; releaseYear = 1984; gross = Nullable<float>() } ]
+        { Id = 1; Name = "The Terminator"; ReleaseYear = 1984; Gross = Nullable 38400000.0 }
+        { Id = 2; Name = "Predator"; ReleaseYear = 1987; Gross = Nullable 59735548.0 }
+        { Id = 3; Name = "Commando"; ReleaseYear = 1985; Gross = Nullable<float>() }
+        { Id = 4; Name = "The Running Man"; ReleaseYear = 1987; Gross = Nullable 38122105.0 }
+        { Id = 5; Name = "Conan the Destroyer"; ReleaseYear = 1984; Gross = Nullable<float>() } ]
     
     let actors =
         [
-        { id = 1; firstName = "Arnold"; lastName = "Schwarzenegger" }
-        { id = 2; firstName = "Linda"; lastName = "Hamilton" }
-        { id = 3; firstName = "Carl"; lastName = "Weathers" }
-        { id = 4; firstName = "Jesse"; lastName = "Ventura" }
-        { id = 5; firstName = "Vernon"; lastName = "Wells" } ]
+        { Id = 1; FirstName = "Arnold"; LastName = "Schwarzenegger" }
+        { Id = 2; FirstName = "Linda"; LastName = "Hamilton" }
+        { Id = 3; FirstName = "Carl"; LastName = "Weathers" }
+        { Id = 4; FirstName = "Jesse"; LastName = "Ventura" }
+        { Id = 5; FirstName = "Vernon"; LastName = "Wells" } ]
 
     let filmActors =
         [ 
-        { filmId = 1; actorId = 1 }
-        { filmId = 1; actorId = 2 }
-        { filmId = 2; actorId = 1 }
-        { filmId = 2; actorId = 3 }
-        { filmId = 2; actorId = 4 }
-        { filmId = 3; actorId = 1 }
-        { filmId = 3; actorId = 5 }
-        { filmId = 4; actorId = 1 }
-        { filmId = 4; actorId = 4 } ]
+        { FilmId = 1; ActorId = 1 }
+        { FilmId = 1; ActorId = 2 }
+        { FilmId = 2; ActorId = 1 }
+        { FilmId = 2; ActorId = 3 }
+        { FilmId = 2; ActorId = 4 }
+        { FilmId = 3; ActorId = 1 }
+        { FilmId = 3; ActorId = 5 }
+        { FilmId = 4; ActorId = 1 }
+        { FilmId = 4; ActorId = 4 } ]
 
 module Program =
     // interface
